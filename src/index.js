ea3e618dafc94156a1423fbace9d478160232235
how-smiple <37428092+muhammedshahid@users.noreply.github.com>
2026-03-26 19:53:04 +0530

    cdn support added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-export { applyPACE } from "./pace.js";
+import { applyPACE } from "./PACE.js";
+
+const PACE = {
+  enhance: applyPACE
+};
+
+// ES module export (for Node / bundlers)
+export { applyPACE };
+export default PACE;
+
+// Browser global (for CDN usage)
+if (typeof window !== "undefined") {
+  window.PACE = PACE;
+}
