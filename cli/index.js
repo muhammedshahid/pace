dfb39663a20a44cf7f5bc78a036567f2206d75d8
how-smiple <37428092+muhammedshahid@users.noreply.github.com>
2026-04-04 20:09:46 +0530

    path.join issue resolved

--- cli/index.js
+++ cli/index.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const pkg = JSON.parse(
-  fs.readFileSync(join(__dirname, "../package.json"), "utf-8")
+  fs.readFileSync(path.join(__dirname, "../package.json"), "utf-8")
 );
 
 const version = pkg.version;
