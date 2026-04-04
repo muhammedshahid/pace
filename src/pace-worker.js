3abfd6601a0cfe64dd3672800c383a0015926fad
how-smiple <37428092+muhammedshahid@users.noreply.github.com>
2026-04-04 17:03:31 +0530

    added pace-worker.js

---
+++ src/pace-worker.js
@@ -0,0 +1,16 @@
+import { applyPACE } from "./PACE.js";
+
+self.onmessage = async (e) => {
+    const { imgData, options } = e.data;
+
+    try {
+        const result = await applyPACE(imgData, options, (progress) => {
+            self.postMessage({ type: 'PROGRESS', data: progress });
+        });
+
+        // zero-copy performance
+        self.postMessage({ type: 'DONE', data: result }, [result.data.buffer]);
+    } catch (error) {
+        self.postMessage({ type: 'ERROR', error: error.message });
+    }
+};
